7279d11a2ee59a9bcaf8c37300e2fc7fe6dc0dfc
preeti <98685188+Priti-parida@users.noreply.github.com>
2025-01-03 10:09:34 +0530

    pms

--- app/Http/Controllers/PmsController.php
+++ app/Http/Controllers/PmsController.php
@@ -51,9 +51,11 @@ class PmsController extends Controller
     ->where('YearId', $year_id)               // Apply the condition for YearId
     ->orderBy('KRASche_DateFrom', 'ASC')      // Order by KRASche_DateFrom in ascending order
     ->get(); 
-    DD($kra_schedule);
+
+        return view("employee.pms");
+
         
-        return view("employee.pmsinfo",compact('exists_appraisel','exists_reviewer','exists_hod','exists_mngmt','kra_schedule'));
+        // return view("employee.pmsinfo",compact('exists_appraisel','exists_reviewer','exists_hod','exists_mngmt','kra_schedule'));
     }
     public function pms(){
         return view("employee.pms");
